09249d055c03907894ba1046da39051eca0b5392
ItsSnowyMoon <henrykeo12@gmail.com>
2025-05-06 00:18:47 -0600

    png image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,17 +12,9 @@ export default function AboutPage() {
     >
       <h1 style={{ fontSize: '2.5rem',color: '#b581d6', marginBottom: '1rem' }}>About Me</h1>
 
-      <p style={{ fontSize: '1.125rem', lineHeight: '1.6'}}>
-        I'm Henry Keo, a UI/UX designer passionate about solving meaningful problems through intuitive and accessible design.
-        With a strong foundation in user research and interaction design, I strive to craft thoughtful digital experiences.
-      </p>
-
-      <p style={{ fontSize: '1.125rem', lineHeight: '1.6', marginTop: '1.5rem'}}>
-        My goal is to continuously learn, iterate, and design with empathy. When I'm not designing, you might find me exploring visual storytelling or tinkering with creative tools.
-      </p>
       <div style={{ marginBottom: '2rem', display: 'flex', justifyContent: 'center' }}>
         <img
-          src="/henrykeocliff.jpg"
+          src="/henrykeocliff.png"
           alt="Henry Keo"
           style={{
             borderRadius: '50%',
@@ -33,6 +25,16 @@ export default function AboutPage() {
           }}
         />
       </div>
+      
+      <p style={{ fontSize: '1.125rem', lineHeight: '1.6'}}>
+        I'm Henry Keo, a UI/UX designer passionate about solving meaningful problems through intuitive and accessible design.
+        With a strong foundation in user research and interaction design, I strive to craft thoughtful digital experiences.
+      </p>
+
+      <p style={{ fontSize: '1.125rem', lineHeight: '1.6', marginTop: '1.5rem'}}>
+        My goal is to continuously learn, iterate, and design with empathy. When I'm not designing, you might find me exploring visual storytelling or tinkering with creative tools.
+      </p>
+     
     </section>
     
   );
